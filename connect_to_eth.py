19ff596d80c0c833e566e18be2e875aabcf2fd75
keem-keem <kareeme@seas.upenn.edu>
2025-03-16 23:20:12 -0400

    Update connect_to_eth.py

--- connect_to_eth.py
+++ connect_to_eth.py
@@ -33,7 +33,7 @@ def connect_with_middleware(contract_json, merkle_contract_json):
 
 	# TODO complete this method
 	# The first section will be the same as "connect_to_eth()" but with a BNB url
-	url = "https://bsc-dataseed.bnbchain.org"
+	url = "https://bsc-testnet-rpc.publicnode.com"
 	w3 = Web3(HTTPProvider(url))
 	assert w3.is_connected(), f"Failed to connect to provider at {url}"
 
