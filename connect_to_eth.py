c60e228947a9e22b3ae6c31b6abdc508369d9ab1
keem-keem <kareeme@seas.upenn.edu>
2025-03-16 22:55:43 -0400

    Update connect_to_eth.py

--- connect_to_eth.py
+++ connect_to_eth.py
@@ -2,7 +2,7 @@ import json
 from web3 import Web3, EthereumTesterProvider, AsyncWeb3
 from web3.middleware import ExtraDataToPOAMiddleware
 from web3.providers.rpc import HTTPProvider
-from web3.middleware.poa import geth_poa_middleware
+from web3.middleware.poa import ExtraDataToPOAMiddleware
 
 '''
 If you use one of the suggested infrastructure providers, the url will be of the form
@@ -28,10 +28,10 @@ def connect_with_middleware(contract_json):
 	# TODO complete this method
 	# The first section will be the same as "connect_to_eth()" but with a BNB url
 	url = "https://bsc-dataseed.binance.org/"
-	w3 = Web3(Web3.HTTPProvider(url))
-	assert w3.is_connected(), f"Failed to connect to provider at {url}"
+	w3 = Web3(EthereumTesterProvider())
+	# assert w3.is_connected(), f"Failed to connect to provider at {url}"
 
-	w3.middleware_onion.inject(geth_poa_middleware, layer=0)
+	w3.middleware_onion.inject(ExtraDataToPOAMiddleware, layer=0)
 
 	# The second section requires you to inject middleware into your w3 object and
 	# create a contract object. Read more on the docs pages at https://web3py.readthedocs.io/en/stable/middleware.html
