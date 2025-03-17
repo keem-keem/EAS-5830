e441b5e9397830f771d4d2c69a6cb4722d7dd8f1
keem-keem <kareeme@seas.upenn.edu>
2025-03-16 22:34:27 -0400

    Update connect_to_eth.py

--- connect_to_eth.py
+++ connect_to_eth.py
@@ -26,9 +26,9 @@ def connect_with_middleware(contract_json):
 
 	# TODO complete this method
 	# The first section will be the same as "connect_to_eth()" but with a BNB url
-	w3 = 0
-	w3 = Web3(EthereumTesterProvider())
+	w3 = Web3(Web3.HTTPProvider('http://127.0.0.1:8545'))
 	w3.is_connected()
+	w3.eth.get_block(‘latest’)
 
 	# The second section requires you to inject middleware into your w3 object and
 	# create a contract object. Read more on the docs pages at https://web3py.readthedocs.io/en/stable/middleware.html
