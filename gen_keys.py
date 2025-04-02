9d753c3d890c81f2b8a9a4fea740eacad48f7292
keem-keem <kareeme@seas.upenn.edu>
2025-04-01 21:41:46 -0400

    Update gen_keys.py

--- gen_keys.py
+++ gen_keys.py
@@ -32,8 +32,8 @@ def sign_message(challenge, filename="secret_key.txt"):
     private_key = account.key
 
     # From signatures.py: sign the given message
-    signed_message = Account.sign_message(challenge, private_key=key)  # Sign the message
-    assert eth_account.Account.recover_message(message,signature=signed_message.signature.hex()) == eth_addr, f"Failed to sign message properly"
+    signed_message = account.Account.sign_message(challenge, private_key=key)  # Sign the message
+    assert account.Account.recover_message(message,signature=signed_message.signature.hex()) == eth_addr, f"Failed to sign message properly"
 
     #return signed_message, account associated with the private key
     return signed_message, eth_addr
