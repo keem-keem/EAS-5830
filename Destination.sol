d47acc561593e14a651d10e68b8429f5f9111c31
keem-keem <kareeme@seas.upenn.edu>
2025-04-09 22:13:21 -0400

    Update Destination.sol

--- Destination.sol
+++ Destination.sol
@@ -25,29 +25,34 @@ contract Destination is AccessControl {
 	function wrap(address _underlying_token, address _recipient, uint256 _amount ) public onlyRole(WARDEN_ROLE) {
 		//YOUR CODE HERE
 		address wrapped = wrapped_tokens[_underlying_token];
+		require(wrapped != address(0), "Underlying token not registered");
 		
+		// Mint BridgeToken to recipient
 		BridgeToken token = BridgeToken(wrapped);
 		token.mint(_recipient, _amount);
-		emit Wrap(_underlying_token, wrapped, _recipient, _amount);
+
+    emit Wrap(_underlying_token, wrapped, _recipient, _amount);
 	}
 
 	function unwrap(address _wrapped_token, address _recipient, uint256 _amount ) public {
 		//YOUR CODE HERE
-		BridgeToken token = BridgeToken(_wrapped_token);
+		address underlying = underlying_tokens[_wrapped_token];
+    		require(underlying != address(0), "Unknown wrapped token");
 
-    		// Burn only if the sender has enough
+    		BridgeToken token = BridgeToken(_wrapped_token);
     		token.burnFrom(msg.sender, _amount);
 
-    		address underlying = underlying_tokens[_wrapped_token];
     		emit Unwrap(underlying, _wrapped_token, msg.sender, _recipient, _amount);
 	}
 
 	function createToken(address _underlying_token, string memory name, string memory symbol ) public onlyRole(CREATOR_ROLE) returns(address) {
 		//YOUR CODE HERE
-		// Deploy a new BridgeToken
-	    	BridgeToken newToken = new BridgeToken(name, symbol);
-	
-	    	// Store mappings
+		require(wrapped_tokens[_underlying_token] == address(0), "Token already registered");
+
+		// Deploy the BridgeToken with msg.sender as admin
+		BridgeToken newToken = new BridgeToken(_underlying_token, name, symbol, address(this));
+		
+		// Save token mappings
 		wrapped_tokens[_underlying_token] = address(newToken);
 		underlying_tokens[address(newToken)] = _underlying_token;
 		tokens.push(address(newToken));
