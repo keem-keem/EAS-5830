57978efd1abf80feea091238ab56ade0a4dcfef0
keem-keem <kareeme@seas.upenn.edu>
2025-04-13 18:42:27 -0400

    Update AMM.sol

--- AMM.sol
+++ AMM.sol
@@ -1,6 +1,5 @@
-
-pragma solidity ^0.8.17;
 // SPDX-License-Identifier: UNLICENSED
+pragma solidity ^0.8.17;
 
 import "@openzeppelin/contracts/access/AccessControl.sol"; //This allows role-based access control through _grantRole() and the modifier onlyRole
 import "@openzeppelin/contracts/token/ERC20/ERC20.sol"; //This contract needs to interact with ERC20 tokens
